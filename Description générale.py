9cd4a913f368871fbcc021e5c3a0b5c081c51064
gregoirehdz <gregoire.hernandez@icloud.com>
2025-11-19 14:25:05 +0100

    Update Description générale.py

--- Description générale.py
+++ Description générale.py
@@ -1,2 +1,3 @@
 trains = {"TUN-PAR": {"places_total": 100,"places_restantes": 100,"passagers": set()},
           "LYO-MRS": {"places_total": 80,"places_restantes": 80,"passagers": set()}}
+plz github
